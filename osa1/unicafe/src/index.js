f26ae31e3b20bf56502e33bb595deb49fd8f093e
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-18 12:14:04 +0300

    unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,29 +1,38 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({good, bad, neutral}) => {
+
+    const total = good+bad+neutral
+    const avg = (good + bad * -1) / total
+    const pos = (good / total) * 100
+
+    return (
+        <>
+        <h1>statistiikka</h1>
+        <p>hyvä {good}</p>
+        <p>neutraali {neutral}</p>
+        <p>huono {bad}</p>
+        <p>yhteensä {total}</p>
+        <p>keskiarvo {avg}</p>
+        <p>positiivisia {pos}%</p>
+        </>
+    )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good+bad+neutral
-  const avg = (good + bad * -1) / total
-  const pos = (good / total) * 100
-;
   return (
     <div>
       <h1>anna palautetta</h1>
       <button onClick={() => setGood(good +1)} >hyvä</button>
       <button onClick={() => setNeutral(neutral+1)}>neutraali </button>
       <button onClick={() => setBad(bad+1)}>huono</button>
-      <h1>statistiikka</h1>
-      <p>hyvä {good}</p>
-      <p>neutraali {neutral}</p>
-      <p>huono {bad}</p>
-      <p>yhteensä {total}</p>
-      <p>keskiarvo {avg}</p>
-      <p>positiivisia {pos}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
