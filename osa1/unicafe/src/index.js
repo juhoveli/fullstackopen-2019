3f6879cb2486e48543e4159bb2b20f0b189566fa
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-18 12:22:33 +0300

    unicafe step4

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = ({good, bad, neutral}) => {
+const Statistics = (props) => {
 
-    const total = good+bad+neutral
-    const avg = (good + bad * -1) / total
-    const pos = (good / total) * 100
-
+  if (props.total === 0) 
     return (
         <>
         <h1>statistiikka</h1>
-        <p>hyvä {good}</p>
-        <p>neutraali {neutral}</p>
-        <p>huono {bad}</p>
-        <p>yhteensä {total}</p>
-        <p>keskiarvo {avg}</p>
-        <p>positiivisia {pos}%</p>
+        <p>Ei yhtään palautetta annettu</p>
         </>
-    )
+      )
+
+  return (
+    <>
+    <h1>statistiikka</h1>
+    <p>hyvä {props.good}</p>
+    <p>neutraali {props.neutral}</p>
+    <p>huono {props.bad}</p>
+    <p>yhteensä {props.total}</p>
+    <p>keskiarvo {props.avg}</p>
+    <p>positiivisia {props.pos}%</p>
+    </>
+  )
 }
 
 const App = () => {
@@ -26,13 +30,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good+bad+neutral
+  const avg = (good + bad * -1) / total
+  const pos = (good / total) * 100
+
   return (
     <div>
       <h1>anna palautetta</h1>
       <button onClick={() => setGood(good +1)} >hyvä</button>
       <button onClick={() => setNeutral(neutral+1)}>neutraali </button>
       <button onClick={() => setBad(bad+1)}>huono</button>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <Statistics good={good} bad={bad} neutral={neutral} 
+      total={total} avg={avg} pos={pos}/>
     </div>
   )
 }
