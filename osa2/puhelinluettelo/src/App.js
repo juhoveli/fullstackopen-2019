2017331ab951b4ddc4631e51b937fb0fbb9bbdb9
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-19 08:11:25 +0300

    puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,34 +2,46 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '050-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const addName = (event) => {
+  const addPerson = (event) => {
       event.preventDefault()
-      const personObject = { name: newName}
+      const personObject = { name: newName, number: newNumber}
       if (persons.find(person => person.name === newName)) {
          alert(`${newName} on jo luettelossa`)
       }
       else {
           setPersons(persons.concat(personObject))
           setNewName('')
+          setNewNumber('')
       }
+  }
+
+  const handleNumberChange = (event) => {
+      setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           nimi: <input 
                   value={newName}
                   onChange={handleNameChange}/>
+        </div>
+        <div>
+            numero: <input
+                      value={newNumber}
+                      onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">lisää</button>
@@ -38,7 +50,7 @@ const App = () => {
       <h2>Numerot</h2>
       <table>
            <tbody>      
-            {persons.map(p => <tr key={p.name}><td>{p.name}</td></tr>)}
+            {persons.map(p => <tr key={p.name}><td>{p.name}</td><td>{p.number}</td></tr>)}
            </tbody>
       </table>
     </div>
