0cf32cbc3c695c2e17492588a92958bc43a28fe6
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-22 22:18:58 +0300

    blogilista step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
   }
   
+  const totalLikes = (blogs) => {
+    const reducer = (acc, cv) => {
+        return acc + cv
+    }
+    const blogLikes = blogs.map(b => b.likes)
+    return blogs.length === 0
+    ? 0
+    : blogLikes.reduce(reducer, 0)
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
+
