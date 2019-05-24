406aa292703cfddff558556a1db5be61215cc877
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-24 18:54:53 +0300

    blogilistan fronted step1

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -1,19 +1,69 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import loginService from './services/login'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+  useEffect(async() => {
+    const blogs = await blogService.getAll()
+    setBlogs(blogs)
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      // TODO: toteuta virheenkäsittely
+    }
+
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({target}) => setUsername(target.value)}
+              />
+          </div>
+          <div>
+            password
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({target}) => setPassword(target.value)}
+              />
+          </div>
+          <button type="submit">log in</button>
+        </form>
+      </div>
+    )
+  }
+
 
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
