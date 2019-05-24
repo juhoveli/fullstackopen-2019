5a4c019fa993a8f073799087afed370786b20e6f
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-24 19:14:34 +0300

    blogilistan fronted step2

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -9,9 +9,19 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
-  useEffect(async() => {
-    const blogs = await blogService.getAll()
-    setBlogs(blogs)
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -21,13 +31,21 @@ const App = () => {
         username, password
       })
 
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       // TODO: toteuta virheenkäsittely
     }
+  }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -64,6 +82,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+      <button onClick={handleLogout}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
