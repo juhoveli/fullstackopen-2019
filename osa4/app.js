1eca4c451c0b62e5ff634896efe8f696f439fb10
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-24 07:46:58 +0300

    blogilistan laajennus step 4

--- osa4/app.js
+++ osa4/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 console.log('connecting to', config.mongoUrl)
@@ -20,6 +21,7 @@ mongoose.connect(config.mongoUrl, { useNewUrlParser: true })
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(cors())
 
 
