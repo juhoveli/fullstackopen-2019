a07edc6ea5987c58b5d1f406955cfa5000ea2055
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-25 20:23:06 +0300

    blogilistan testit step2

--- osa5/src/components/SimpleBlog.test.js
+++ osa5/src/components/SimpleBlog.test.js
@@ -5,12 +5,12 @@
 
 import React from 'react'
 import 'jest-dom/extend-expect'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
 
-test('SimpleBlog renders content', () => {
+test('renders content', () => {
   const blog = {
     title: 'Not a Blog',
     author: 'George R.R. Martin',
@@ -32,4 +32,24 @@ test('SimpleBlog renders content', () => {
   expect(component.container).toHaveTextContent(
     '99'
   )
+})
+
+test('like button calls event handler', () => {
+  const blog = {
+    title: 'Not a Blog',
+    author: 'George R.R. Martin',
+    likes: 99
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
