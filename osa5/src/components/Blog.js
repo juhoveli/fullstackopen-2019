78811d4d9d956002d3a43af5b52ad4cc470f6cbf
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-25 11:10:05 +0300

    blogilistan frontend step6

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,23 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const [blogShowDetails, setBlogShowDetails] = useState(false)
+
+  const noDetails = { display: blogShowDetails? 'none' : '' }
+  const allDetails = { display: blogShowDetails ? '' : 'none' }
+
+  return (
+  <div className="blog">
+    <p onClick={() => setBlogShowDetails(!blogShowDetails)}><b>{blog.title}</b> {blog.author}</p>
+    <div style={noDetails}>
+    </div>
+    <div style={allDetails}>
+      <a href={blog.url}>{blog.url}</a>
+      <p>{blog.likes} likes <button>like</button></p> 
+      <p>added by {blog.user.name}</p>
+    </div>
   </div>
 )
+}
 
 export default Blog
